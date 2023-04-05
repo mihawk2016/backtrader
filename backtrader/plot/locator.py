cd4a3b9fe3c6b4f37ab1607c2feebcba15ee621f
zhangzhang <mihawk20101@gmail.com>
2023-04-05 15:53:01 +0800

    update

--- backtrader/plot/locator.py
+++ backtrader/plot/locator.py
@@ -223,8 +223,8 @@ class AutoDateLocator(ADLocator):
 
         locator.set_axis(self.axis)
 
-        locator.set_view_interval(*self.axis.get_view_interval())
-        locator.set_data_interval(*self.axis.get_data_interval())
+        # locator.set_view_interval(*self.axis.get_view_interval()) 注释消除报错
+        # locator.set_data_interval(*self.axis.get_data_interval()) 注释消除报错
         return locator
 
 
